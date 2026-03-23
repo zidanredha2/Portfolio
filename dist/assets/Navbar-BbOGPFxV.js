0a6ed9678ad72df30a80906aa96d830637d1b595
zidanredha2 <amazinginflation58@gmail.com>
2026-03-24 00:08:45 +0530

    fix base for gh pages

--- dist/assets/Navbar-BbOGPFxV.js
+++ dist/assets/Navbar-BbOGPFxV.js
@@ -1,4 +1,4 @@
-import{j as S,r as Be}from"./index-Dr56vYCL.js";import{g as Ae,S as xe}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j as S,r as Be}from"./index-BBC5an5M.js";import{g as Ae,S as xe}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.12.7
  * https://gsap.com
  * 
